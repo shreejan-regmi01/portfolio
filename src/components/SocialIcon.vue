0938e397ac4f9d9fdf5a487ade080610d6097a5d
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-08-16 22:03:54 +0545

    finally fixed hover on social icons

--- src/components/SocialIcon.vue
+++ src/components/SocialIcon.vue
@@ -1,9 +1,5 @@
 <template>
-  <base-highlight
-    class="inline-block px-3 py-2 rounded-lg hover:!text-white"
-    :style="{ color }"
-    :class="background"
-  >
+  <base-highlight class="inline-block px-3 py-2 rounded-lg custom" :style="cssProps">
     <slot></slot>
   </base-highlight>
 </template>
@@ -14,13 +10,21 @@ export default {
   computed: {
     background() {
       return `hover:bg-[${this.color}]`
+    },
+    cssProps() {
+      return { '--text-color': this.color }
     }
   }
 }
 </script>
 
 <style scoped>
-/* .rounded {
-  background-color: #0a66c2;
-} */
+.custom {
+  color: var(--text-color);
+}
+
+.custom:hover {
+  color: white;
+  background-color: var(--text-color);
+}
 </style>
