6db546c24922d9a8f0eee75ac88a9e8cd23ff6a6
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-08-17 22:34:30 +0545

    added redirect to homepage

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: { name: 'about' }
+    },
+    {
       path: '/about',
       name: 'about',
       component: AboutView
