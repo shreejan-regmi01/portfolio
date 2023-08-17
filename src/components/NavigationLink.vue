78ddf3de2384bc6a97dae06e9aefcd2d2f9f469d
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-08-17 21:54:36 +0545

    made navigation link component

---
+++ src/components/NavigationLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-highlight
+    class="p-4 rounded-xl hover:bg-accent-blue hover:text-white text-secondary"
+    :class="active && 'active'"
+  >
+    <div>
+      <slot></slot>
+    </div>
+    <p class="mt-2 text-sm font-medium" :class="customClassForLabel">{{ label }}</p>
+  </base-highlight>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      required: false
+    },
+    customClassForLabel: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
--- src/components/layout/TheNavigation.vue
+++ src/components/layout/TheNavigation.vue
@@ -1,39 +1,31 @@
 <template>
   <base-container class="p-7 text-center h-min flex flex-col gap-5">
-    <base-highlight class="p-4 rounded-xl active">
-      <div>
-        <i class="fa-regular fa-user fa-xl"></i>
-      </div>
-      <p class="mt-2 text-sm font-medium">About</p>
-    </base-highlight>
-    <base-highlight class="p-4 rounded-xl text-secondary hover:bg-accent-blue hover:text-white">
-      <div>
-        <i class="fa-regular fa-file fa-xl"></i>
-      </div>
-      <p class="mt-2 text-sm font-medium">Resume</p>
-    </base-highlight>
-    <base-highlight class="p-4 rounded-xl text-secondary hover:bg-accent-blue hover:text-white">
-      <div>
-        <i class="fa-solid fa-toolbox fa-xl"></i>
-      </div>
-      <p class="mt-2 text-sm font-medium">Work</p>
-    </base-highlight>
-    <base-highlight class="p-4 rounded-xl text-secondary hover:bg-accent-blue hover:text-white">
-      <div>
-        <i class="fa-regular fa-address-book fa-xl"></i>
-      </div>
-      <p class="mt-2 text-sm font-medium">Contact</p>
-    </base-highlight>
-    <base-highlight class="p-4 rounded-xl text-secondary hover:bg-accent-blue hover:text-white">
-      <div>
-        <i class="fa-regular fa-thumbs-up fa-xl"></i>
-      </div>
-      <p class="mt-2 text-sm font-medium -ml-2">Testimonials</p>
-    </base-highlight>
+    <navigation-link label="About" :active="true">
+      <i class="fa-regular fa-user fa-xl"></i>
+    </navigation-link>
+    <navigation-link label="Resume">
+      <i class="fa-regular fa-file fa-xl"></i>
+    </navigation-link>
+    <navigation-link label="Work">
+      <i class="fa-solid fa-toolbox fa-xl"></i>
+    </navigation-link>
+    <navigation-link label="Contact">
+      <i class="fa-regular fa-address-book fa-xl"></i>
+    </navigation-link>
+    <navigation-link label="Testimonials" customClassForLabel="-ml-2">
+      <i class="fa-regular fa-thumbs-up fa-xl"></i>
+    </navigation-link>
   </base-container>
 </template>
 
-<script></script>
+<script>
+import NavigationLink from '../NavigationLink.vue'
+export default {
+  components: {
+    NavigationLink
+  }
+}
+</script>
 
 <style scoped>
 .active {
