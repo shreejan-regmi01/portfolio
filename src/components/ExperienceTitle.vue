3be05b43ae6ae27a95f4f16bad82ccdf4b53c210
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-08-28 23:05:37 +0545

    completed layout of exp vie

---
+++ src/components/ExperienceTitle.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2
+    class="font-semibold font-secondary text-xl py-2 bg-dark-primary absolute left-1/2 -ml-[54px]"
+  >
+    <slot></slot>
+  </h2>
+</template>
--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,23 +1,18 @@
 <template>
   <base-container class="pt-16 pb-8 px-14">
     <main-title headerTitle="Education & Experience" class="mb-10"></main-title>
+    <icon-heading label="Education" class="mb-8">
+      <i class="fa-solid fa-graduation-cap fa-xl"></i>
+    </icon-heading>
     <experience-layout>
-      <h2
-        class="font-semibold font-secondary text-xl py-2 bg-dark-primary absolute left-1/2 -ml-[54px]"
-      >
-        2014 - 2016
-      </h2>
+      <experience-title> 2014 - 2016 </experience-title>
       <ExperienceDescription
         title="high school"
         subtitle="arniko higher secondary school"
         description="science"
       ></ExperienceDescription>
       <div class="mt-10"></div>
-      <h2
-        class="font-semibold font-secondary text-xl py-2 bg-dark-primary absolute left-1/2 -ml-[54px]"
-      >
-        2017 - 2020
-      </h2>
+      <experience-title> 2017 - 2020 </experience-title>
       <ExperienceDescription
         title="bachelor's degree"
         subtitle="university of northampton"
@@ -28,16 +23,37 @@
         dolorem?
       </ExperienceDescription>
     </experience-layout>
+    <icon-heading label="Experience" class="mt-16 mb-8">
+      <i class="fa-solid fa-briefcase fa-xl"></i>
+    </icon-heading>
+    <experience-layout>
+      <experience-title>2022 - Present</experience-title>
+      <ExperienceDescription
+        title="Automation/Functional QA Engineer"
+        subtitle="CarClarity"
+        description="responsibilities"
+      ></ExperienceDescription>
+      <div class="mt-10"></div>
+      <experience-title>2021 - 2022</experience-title>
+      <ExperienceDescription title="QA Engineer" subtitle="WhiteHat Engineering" :rotate="true">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum odio magnam numquam nemo
+        dolorem?
+      </ExperienceDescription>
+    </experience-layout>
   </base-container>
 </template>
 
 <script>
 import ExperienceDescription from '../components/ExperienceDescription.vue'
 import ExperienceLayout from '../components/layout/ExperienceLayout.vue'
+import IconHeading from '../components/IconHeading.vue'
+import ExperienceTitle from '../components/ExperienceTitle.vue'
 export default {
   components: {
     ExperienceDescription,
-    ExperienceLayout
+    ExperienceLayout,
+    IconHeading,
+    ExperienceTitle
   }
 }
 </script>
