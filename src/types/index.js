33ab3366d7adfd36b4ffb417308e9e0b4da63097
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-09-23 22:13:44 +0545

    added validations to 3 fields

---
+++ src/types/index.js
@@ -0,0 +1,4 @@
+export const FIRSTNAME_ERR = 'Firstname is required'
+export const LASTNAME_ERR = 'Lastname is required'
+export const EMAIL_EMPTY_ERR = 'Email is required'
+export const EMAIL_INVALID_ERR = 'Invalid email format'
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -16,8 +16,14 @@
           id="lastname"
           :error="errors.lastname"
         ></base-input>
-        <base-input v-model="email" label="Email" id="email"></base-input>
-        <base-input v-model="mobile" label="Mobile" id="mobile" type="number"></base-input>
+        <base-input v-model="email" label="Email" id="email" :error="errors.email"></base-input>
+        <base-input
+          v-model="mobile"
+          label="Mobile"
+          id="mobile"
+          type="number"
+          :error="errors.mobile"
+        ></base-input>
         <base-text-area v-model="message" label="Message" class="col-span-2"></base-text-area>
         <base-button
           label="Download CV"
@@ -29,6 +35,7 @@
 </template>
 
 <script>
+import { FIRSTNAME_ERR, LASTNAME_ERR, EMAIL_EMPTY_ERR, EMAIL_INVALID_ERR } from '../types'
 export default {
   data() {
     return {
@@ -38,11 +45,29 @@ export default {
       mobile: '',
       message: '',
       errors: {
-        firstname: 'firstname',
+        firstname: '',
         lastname: '',
         email: '',
         mobile: ''
       }
+    }
+  },
+  watch: {
+    firstname(newVal) {
+      if (newVal.trim() == '') return (this.errors.firstname = FIRSTNAME_ERR)
+      this.errors.firstname = ''
+    },
+    lastname(newVal) {
+      if (newVal.trim() == '') return (this.errors.lastname = LASTNAME_ERR)
+      this.errors.lastname = ''
+    },
+    email(newVal) {
+      if (newVal.trim() == '') return (this.errors.email = EMAIL_EMPTY_ERR)
+      // eslint-disable-next-line no-useless-escape
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(newVal)) {
+        return (this.errors.email = EMAIL_INVALID_ERR)
+      }
+      this.errors.email = ''
     }
   }
 }
