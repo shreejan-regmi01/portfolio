8f54164fa17a87a0a56cabf38f4f1773d5c8414c
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-09-27 23:46:50 +0545

    updated padding

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,5 +1,5 @@
 <template>
-  <base-container class="pt-16 pb-8 px-6 xl:px-14">
+  <base-container class="px-6 py-8 xl:pt-16 xl:pb-8 xl:px-14">
     <main-title headerTitle="Education & Experience" class="mb-10"></main-title>
     <icon-heading label="Education" class="mb-8">
       <i class="fa-solid fa-graduation-cap fa-xl"></i>
