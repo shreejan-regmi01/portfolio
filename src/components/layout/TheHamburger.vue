b68475a0298a7797ea5196b2a435aa475d160df9
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-10-01 23:39:15 +0545

    added hamburger design

---
+++ src/components/layout/TheHamburger.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-12 h-11 px-3 py-3 rounded-full bg-black grid gap-y-[5px] grid-cols-1 grid-rows-3">
+    <div class="h-[3px] bg-white"></div>
+    <div class="h-[3px] bg-white"></div>
+    <div class="h-[3px] bg-white"></div>
+  </div>
+</template>
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,16 +1,23 @@
 <template>
-  <header class="w-40 xl:w-60 ml-5">
-    <img :src="logo" alt="" />
+  <header class="ml-5 flex justify-between items-center">
+    <div class="w-40 xl:w-60">
+      <img :src="logo" alt="" />
+    </div>
+    <the-hamburger class="xl:hidden"></the-hamburger>
   </header>
 </template>
 
 <script>
 import logo from '../../assets/logo-transparent.svg'
+import TheHamburger from './TheHamburger.vue'
 export default {
   data() {
     return {
       logo
     }
+  },
+  components: {
+    TheHamburger
   }
 }
 </script>
