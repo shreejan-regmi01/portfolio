bf7e85230852c58a45cac6563c43f6f08584f688
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-10-05 21:07:34 +0545

    added hamburger movement to bottom of the screen on scroll

--- src/components/layout/TheHamburger.vue
+++ src/components/layout/TheHamburger.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="w-12 h-11 px-3 py-3 rounded-full bg-black grid gap-y-[5px] grid-cols-1 grid-rows-3">
-    <div class="h-[3px] bg-white"></div>
-    <div class="h-[3px] bg-white"></div>
-    <div class="h-[3px] bg-white"></div>
+  <div
+    class="w-12 h-11 px-3 py-3 rounded-full bg-black grid gap-y-[5px] grid-cols-1 grid-rows-3"
+    :class="classForBottomHamburger"
+    ref="hamburger"
+  >
+    <div class="h-[3px] bg-white rounded-sm"></div>
+    <div class="h-[3px] bg-white rounded-sm"></div>
+    <div class="h-[3px] bg-white rounded-sm"></div>
   </div>
 </template>
+
+<script>
+//todo: move hamburger to bottom on scroll ref: https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp
+import { useScreen } from '../../composables/useScreen'
+export default {
+  data() {
+    return {
+      displayHamburgerAtBottom: false
+    }
+  },
+  computed: {
+    classForBottomHamburger() {
+      return this.displayHamburgerAtBottom
+        ? `fixed bottom-8 right-8 !bg-accent-blue transition duration-300 ease-in-out`
+        : null
+    }
+  },
+  mounted() {
+    if (this.isMobile()) {
+      window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 85) {
+          return (this.displayHamburgerAtBottom = true)
+        }
+        return (this.displayHamburgerAtBottom = false)
+      })
+    }
+  },
+  setup() {
+    const { isMobile } = useScreen()
+    return {
+      isMobile
+    }
+  }
+}
+</script>
